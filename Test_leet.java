3af45a918f21b24d0af90c292beedaec0ba5dd80
Wu Xudong <wuxudong25@gmail.com>
2017-11-19 23:44:19 +0800

    add two sum solution

---
+++ Test_leet.java
@@ -0,0 +1,85 @@
+package test_leet;
+import java.lang.String;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Test_leet {
+	public boolean isBadVersion(int n){
+		if (n<100){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+	
+    public int firstBadVersion(int n) {
+        return checkVersion(1,n);
+    }
+    
+    public int checkVersion(int a, int b){
+        if ((b-a) == 1){
+            if (isBadVersion(a)){
+                return a;
+            }
+            else{
+                return b;
+            }
+        }
+        else{
+        	if (isBadVersion(a+(b-a)/2)){
+        		return checkVersion(a, a+(b-a)/2);
+        	}
+        	else{
+        		return checkVersion(a+(b-a)/2, b);
+        	}
+        }
+    }
+    
+
+
+	public static void main(String[] args) {
+		//Test_leet test_leet = new Test_leet();
+		//System.out.println(test_leet.firstBadVersion(277));
+		 
+        Map map=new HashMap();                
+        map.put("a", "aaa");    
+        map.put("b", "bbb");    
+        map.put("c", "ccc");    
+        map.put("d", "ddd");    
+           
+        Iterator iterator = map.keySet().iterator();                
+        while (iterator.hasNext()) {    
+         Object key = iterator.next();    
+         System.out.println("map.get(key) is :"+map.get(key));    
+        }           
+            
+            
+            
+        Hashtable tab=new Hashtable();                
+        tab.put("a", "aaa");    
+        tab.put("b", "bbb");    
+        tab.put("c", "ccc");    
+        tab.put("d", "ddd");    
+        Iterator iterator_1 = tab.keySet().iterator();    
+        while (iterator_1.hasNext()) {    
+         Object key = iterator_1.next();    
+         System.out.println("tab.get(key) is :"+tab.get(key));    
+        }             
+            
+        TreeMap tmp=new TreeMap();                
+        tmp.put("a", "aaa");    
+        tmp.put("b", "bbb");    
+        tmp.put("c", "ccc");    
+        tmp.put("d", "ddd");    
+        Iterator iterator_2 = tmp.keySet().iterator();    
+        while (iterator_2.hasNext()) {    
+         Object key = iterator_2.next();    
+         System.out.println("tmp.get(key) is :"+tmp.get(key));    
+        }             
+                      
+	}
+}
---
+++ TwoSum.java
@@ -0,0 +1,36 @@
+package test_leet;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+	public int[] twoSum(int[] nums, int target)
+	{
+		Map<Integer, Integer> toSaveMap = new HashMap<>();
+		for(int i=0;i<nums.length;i++)
+		{
+			if (toSaveMap.containsKey(nums[i]))
+			{
+				int[] result = {toSaveMap.get(nums[i]).intValue(), i};
+				return result;
+			}
+			else
+			{
+				toSaveMap.put(target-nums[i], i);
+			}
+		}
+		
+		return new int[]{0,0};
+	}
+	
+	public static void main(String[] args) {
+		TwoSum example = new TwoSum();
+		int[] arrayList = {2,11,15,0,19,20,7};
+		int target = 9;
+		int[] resultArray = example.twoSum(arrayList, target);
+		for (int i:resultArray)
+		{
+			System.out.println(i);
+		}
+	}
+}
