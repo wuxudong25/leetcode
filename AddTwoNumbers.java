e3b9801f9a8d823411f6d3c793c827344cfa07a3
Wu Xudong <wuxudong25@gmail.com>
2017-12-11 21:05:01 +0800

    add one more solution

---
+++ AddTwoNumbers.java
@@ -0,0 +1,70 @@
+package test_leet;
+
+public class AddTwoNumbers {
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
+
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		// add the current val while any of the node is not null, and add the carry value to the next
+		ListNode resultNode = new ListNode(-1);
+		ListNode currentNode = resultNode;
+		int sum = 0;
+		int carry = 0;
+		if (l1 == null && l2 == null) {
+			return resultNode;
+		}
+		while (l1!=null || l2!=null) {
+			int a = l1!=null ? l1.val:0;
+			int b = l2!=null ? l2.val:0;
+			sum = carry + a + b;
+			currentNode.next = new ListNode(sum%10);
+			carry = sum/10;
+			currentNode = currentNode.next;
+			l1 = l1!=null&&l1.next!=null ? l1.next:null;
+			l2 = l2!=null&&l2.next!=null ? l2.next:null;
+		}
+		
+		if (carry>0) {
+			currentNode.next = new ListNode(carry);
+		}
+		
+		if (resultNode.next!=null) {
+			resultNode = resultNode.next;
+		}
+		
+		return resultNode;
+	}
+	
+	public static void main(String[] args) {
+		AddTwoNumbers example = new AddTwoNumbers();
+		int [] alist = {2,4,5};
+		int [] blist = {1,5,4,4};
+		ListNode aListNode = example.new ListNode(0);
+		ListNode bListNode = example.new ListNode(0);
+		ListNode anode = aListNode;
+		ListNode bnode = bListNode;
+
+		for (int i : alist){
+			anode.next = example.new ListNode(i);
+			anode=anode.next;
+		}
+		for (int i : blist){
+			bnode.next = example.new ListNode(i);
+			bnode=bnode.next;
+		}
+		aListNode = aListNode.next;
+		bListNode = bListNode.next;
+		
+		ListNode resultNode = example.addTwoNumbers(aListNode,bListNode);
+		while(resultNode!=null){
+			System.out.println(resultNode.val);
+			resultNode=resultNode.next;
+		}
+	}
+}
---
+++ LongestSubstring.java
@@ -0,0 +1,5 @@
+package test_leet;
+
+public class LongestSubstring {
+
+}
